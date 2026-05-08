e6ee1eb098637511606c95c78a0b2ce0d1f5fb8b
Satyapalk <satu8773@gmail.com>
2026-05-08 14:35:25 +0700

    add web icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Satya Portfolio",
   description: "Full-stack developer portfolio showcasing my projects and skills",
+  icons: {
+  icon: "/icon.png",
+},
 };
 
 export default function RootLayout({
